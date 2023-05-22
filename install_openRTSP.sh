392d2b62c2c2495fc1e0458b0b016e8d3eabd803
Hari Narasimhan <36240321+narasimhan3737@users.noreply.github.com>
2023-05-22 01:24:51 +0100

    Update install_openRTSP.sh

--- install_openRTSP.sh
+++ install_openRTSP.sh
@@ -1,5 +1,5 @@
 cd /usr/src
-wget http://www.live555.com/liveMedia/public/live555-latest.tar.gz
+sudo wget http://www.live555.com/liveMedia/public/live555-latest.tar.gz
 sudo tar -xzf live555-latest.tar.gz
 cd live
 sudo ./genMakefiles linux
